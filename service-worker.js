323097ad615fb674af23d90be770773223049674
Nathannael <nathannael2009@gmail.com>
2019-10-07 23:59:54 -0300

    update 07/10

--- service-worker.js
+++ service-worker.js
@@ -1,29 +1,23 @@
-/**
- * Welcome to your Workbox-powered service worker!
- *
- * You'll need to register this file in your web app and you should
- * disable HTTP caching for this file too.
- * See https://goo.gl/nhQhGp
- *
- * The rest of the code is auto-generated. Please don't update this file
- * directly; instead, make changes to your Workbox build configuration
- * and re-run your build process.
- * See https://goo.gl/2aRDsh
- */
+importScripts("/precache-manifest.f5a15ebec1f7182c3eceb7a680d25454.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
-
-importScripts(
-  "/precache-manifest.3654b095ca557e2f6cfcbe3bdee19aa8.js"
-);
-
-workbox.core.setCacheNameDetails({prefix: "gdg-web-app"});
-
-/**
- * The workboxSW.precacheAndRoute() method efficiently caches and responds to
- * requests for URLs in the manifest.
- * See https://goo.gl/S9QRab
- */
-self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+self.addEventListener('message', (e) => {
+    if (!e.data) {
+      return;
+    }
+  
+    switch (e.data) {
+      case 'skipWaiting':
+        self.skipWaiting();
+        break;
+      default:
+        // NOOP
+        break;
+    }
+  });
+  
+  workbox.clientsClaim();
+  
+  // The precaching code provided by Workbox.
+  self.__precacheManifest = [].concat(self.__precacheManifest || []);
+  workbox.precaching.suppressWarnings();
+  workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
